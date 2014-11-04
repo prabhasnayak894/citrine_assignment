b936de8af1337ef02cc279c1cc42d0eb7228f579
prabhasnayak894 <Prabhas.Nayak@e-zest.in>
2014-11-04 17:28:40 +0530

    Updated Code

--- app/scripts/controllers/view_timesheets.js
+++ app/scripts/controllers/view_timesheets.js
@@ -8,10 +8,16 @@
  * Controller of the codeApp
  */
 angular.module('codeApp')
-  .controller('ViewTimesheetsCtrl', function ($scope) {
+  .controller('ViewTimesheetsCtrl', ['$scope', 'timeEntries', function ($scope, timeEntries) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+   	timeEntries.get_timeEnteries();
+   	$scope.details = timeEntries.timeEnteries;
+   /* $scope.showDetails = function() {
+    	console.log("shdfgbiuyshdfb")
+   		timeEntries.get_timeEnteries();
+    };*/
+  }]);
--- app/scripts/services/time_entries.js
+++ app/scripts/services/time_entries.js
@@ -8,6 +8,24 @@
  * Service in the codeApp.
  */
 angular.module('codeApp')
-  .service('timeEntries', function timeEntries() {
+  .service('timeEntries',['$http', function timeEntries( $http) {
     // AngularJS will instantiate a singleton by calling "new" on this function
-  });
+
+    this.timeEnteries = {};
+    this.get_timeEnteries = function() {
+    	var details = this;
+    	try {
+	        $http.get('scripts/details.json').success(function (data) {
+	        	details.timeEnteries = data.details;
+	        })
+	        .error(function () {
+	        })
+	        .then(function () {
+
+	        });                     
+	    }
+	    catch (eX) {
+	        console.dir(eX);
+	    }
+    }
+  }]);
