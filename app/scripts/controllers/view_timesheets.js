387021441b91f180e3bde8e9352e7bb3bd6441f9
prabhas125 <prabhas.nayak@gmail.com>
2014-11-05 14:50:06 +0530

    Sorting of table columns

--- app/scripts/controllers/view_timesheets.js
+++ app/scripts/controllers/view_timesheets.js
@@ -10,4 +10,28 @@
 angular.module('codeApp')
   .controller('ViewTimesheetsCtrl', ['$scope', 'timeEntries', function ($scope, timeEntries) {
     $scope.entries = timeEntries.entries;
+    $scope.total_hrs = 0;
+    $scope.total_mins = 0 ; 
+
+    $scope.$watch($scope.entries, function( newValue, oldValue ) {
+
+      for (var i in $scope.entries) {
+        var detail = $scope.entries[i];
+        var hrs = detail.duration.split(" ");
+        $scope.total_hrs += parseInt(hrs[0],10);
+
+        if (hrs[2]) {
+          $scope.total_mins += parseInt(hrs[2],10);  
+          if ($scope.total_mins >= 60) {
+            $scope.total_hrs += parseInt($scope.total_mins/60);
+            $scope.total_mins = $scope.total_mins%60;
+          }
+        }
+        
+      };
+    });
+   
+    $scope.changeSorting = function(column) {
+        
+    };
   }]);
